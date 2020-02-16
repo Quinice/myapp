e5607d9ec0b3f02d0ee7523afca96d41d0e79be7
QQJ <34911608QQJ@users.noreply.github.com>
2020-02-16 11:27:15 +0800

    test
    熟悉下github更新

---
+++ My APP8/app/src/main/java/com/example/myapp8/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,21 @@
+package com.example.myapp8.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapp8.R;
+
+public class DashboardFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        return root;
+    }
+}
